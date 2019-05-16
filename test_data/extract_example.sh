749554d74e8d86229b7a2dff1e8b931fc2fcfdfd
Daniel Putnam <putnamdk@users.noreply.github.com>
2019-05-16 17:41:44 -0500

    Add files via upload

---
+++ test_data/extract_example.sh
@@ -0,0 +1,13 @@
+tar -xf xaa.tar.xz
+tar -xf xab.tar.xz
+tar -xf xac.tar.xz
+tar -xf xad.tar.xz
+tar -xf xae.tar.xz
+tar -xf xaf.tar.xz
+tar -xf xag.tar.xz
+tar -xf xah.tar.xz
+tar -xf xai.tar.xz
+tar -xf xaj.tar.xz
+tar -xf xak.tar.xz
+
+cat xaa xab xac xad xae xaf xag xah xai xaj xak > test.high_20.out
